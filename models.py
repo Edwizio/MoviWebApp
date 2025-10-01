5f5c689bede24e2deae1192021194fb7a02612e5
Zahra Rauf <zahra.aleem@gmail.com>
2025-10-01 12:21:10 +0200

    models.py defined with basic User and Movie class definitions

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+# Defining the User model as a class
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+# Defining the Movie model as a class
+class Movie(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String (200), nullable=False)
+  director = db.Column(db.String (200), nullable=False)
+  year = db.Column(db.Integer, nullable=False)
+  poster_url = db.Column(db.String (500), nullable=False)
+
+  # Link Movie to User
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
